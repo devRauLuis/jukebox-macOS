71186f6f868fae4471e7fbe6af5f0d8148842144
Raul Luis <soyrauluis@gmail.com>
2023-03-05 01:06:15 -0400

    Add TrackRow

---
+++ Jukebox/Views/TrackRow.swift
@@ -0,0 +1,49 @@
+//
+//  QueueRow.swift
+//  Jukebox
+//
+//  Created by Raúl Luis on 2/3/23.
+//
+
+import SwiftUI
+
+struct TrackRow: View {
+    var track: Track
+    var handleAdd: () -> Void
+
+    var body: some View {
+        HStack {
+            Base64ImageView(base64: track.image, defaultImageIconSize: 30).frame(width: 50, height: 50)
+            VStack(alignment: .leading) {
+                Text(track.title)
+                        .font(.system(size: 16))
+                        .bold()
+                        .lineLimit(1)
+                        .truncationMode(.tail)
+                Text(track.artist).font(.subheadline)
+            }
+            Spacer()
+            Button(action: handleAdd) {
+                Image(systemName: "text.badge.plus").foregroundColor(.red).font(.system(size: 12))
+            }
+                    .buttonStyle(.plain)
+        }
+                .padding([.top, .bottom]).onAppear {
+                    print("track image \(track.image)")
+                }
+    }
+}
+
+struct TrackRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackRow(track: Track(songName: "01",
+                title: "Days Before Rodeo The Prequel",
+                trackNumber: "1",
+                genre: "",
+                artist: "Travis Scott",
+                releaseTime: "Travi$ Scott",
+                year: "Travi$ Scott",
+                album: "Days Before Rodeo",
+                image: "iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAIAAADTED8xAAADMElEQVR4nOzVwQnAIBQFQYXff81RUkQCOyDj1YOPnbXWPmeTRef+/3O/OyBjzh3CD95BfqICMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMO0TAAD//2Anhf4QtqobAAAAAElFTkSuQmCC", posId: "abcdefghijk"), handleAdd: {})
+    }
+}
